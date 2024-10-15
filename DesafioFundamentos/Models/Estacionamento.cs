e5a6a21d8f002c2dfdfa23148c99d9693e9e8f11
Kienner <kennerdev@outlook.com>
2024-10-15 18:00:29 -0300

    clean code

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -14,8 +14,6 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
             string entradaPlaca; 
 
             Console.WriteLine("Digite a placa do veículo para estacionar:");
@@ -35,9 +33,6 @@ namespace DesafioFundamentos.Models
 
             Console.WriteLine("Digite a placa do veículo para remover:");
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            
             string placa = "";
 
             placa = Console.ReadLine();
@@ -51,16 +46,9 @@ namespace DesafioFundamentos.Models
 
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
                 horas = int.Parse(Console.ReadLine());
-                
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                
 
                 valorTotal = (precoInicial + precoPorHora) * horas ;
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
                 veiculos.Remove(placa);
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
@@ -77,8 +65,7 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+
                 foreach (string veiculo in veiculos){
                     Console.WriteLine($" Placa|{veiculo}");
                 }
